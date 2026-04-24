9856ed8e219c8d4a4bbd80ab7a4045b198113a11
tanbjs <benjaset.m@gmail.com>
2026-04-24 20:12:53 +0700

    fix: correct script paths in run_local.sh (src/ -> script/)

--- sysid/run_local.sh
+++ sysid/run_local.sh
@@ -1,9 +1,9 @@
-set -a 
+set -a
 source .env
 set +a
 
-run_id=$(python src/setup.py --model_config config/kaec/edmdc_config.yaml | tail -n 1)
+run_id=$(python script/setup.py --model_config config/kaec/edmdc_config.yaml | tail -n 1)
 echo "Run ID: $run_id"
-python src/process.py --run_id $run_id
-python src/train.py --run_id $run_id
-python src/validate.py --run_id $run_id --n_test 10
+python script/process.py --run_id $run_id
+python script/train.py --run_id $run_id
+python script/validate.py --run_id $run_id --n_test 10
